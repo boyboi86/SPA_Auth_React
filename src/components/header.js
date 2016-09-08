fef69312edcbbc35f7c851fef2ca74fdad0f7df6
boyboi86 <boyboi86@gmail.com>
2016-09-08 20:16:05 +0800

    connect state/dispatch to props

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class Header extends Component{
+class Header extends Component{
 
 authButton(){
+    if(this.props.authenticated){
+      return (
+        <button onClick={ () => this.props.authenticate(false)} >Sign Out</button>
+      )
+    }
   return(
-    <button>Sign in</button>
+    <button onClick={ () => this.props.authenticate(true)}  >Sign in</button>
   )
 }
 
@@ -21,3 +28,9 @@ authButton(){
     )
   }
 }
+
+function mapStateToProps(state){
+  return { authenticated: state.authenticated }
+}
+
+export default connect(mapStateToProps, actions)(Header)
