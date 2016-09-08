6ee9e161d025ba6fc5b00b9ce825fbf27134bda9
boyboi86 <boyboi86@gmail.com>
2016-09-08 19:40:20 +0800

    created secret route

---
+++ src/components/resources.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div>
+      <h3>Secret Route</h3>
+      <div>If you can see this, something is wrong with my routing</div>
+    </div>
+  )
+}
