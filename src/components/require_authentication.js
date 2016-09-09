34115e84e55dae5d546fd1dbe7f10cf37effa1c0
boyboi86 <boyboi86@gmail.com>
2016-09-09 16:51:44 +0800

    ensured that login or out is essential for view layer

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,9 +3,25 @@ import { connect } from 'react-redux';
 
 export default function (ComposedComponent) {
   class Authentication extends Component{
+
+/*Defined contextTypes to access context */
     static contextTypes = {
         router: React.PropTypes.object
     }
+
+/*Made used of context to define route*/
+    componentWillMount(){
+      if(!this.props.authenticated){
+        this.context.router.push("/");
+      }
+    }
+
+/*If component update check if user still sign in */
+  componentWillUpdate(nextProps){
+    if(!nextProps.authenticated){
+      this.context.router.push("/");
+    }
+  }
 
     render(){
       return <ComposedComponent { ...this.props } />
